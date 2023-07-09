3c4835ab3b946b31f92721e153dd4cb12be5e145
wisdom-plus <tomoron326@outlook.jp>
2023-07-09 10:58:44 +0900

    get selection

--- app.rb
+++ app.rb
@@ -1,7 +1,39 @@
 require 'unloosen'
 
-content_script site: 'www.example.com' do
-  h1 = document.querySelector('h1')
-  h1.innerText = "Unloosen Example Page!"
+class Utils
+  def self.build_js_object(**kwargs)
+    # This is workaround: in popup, JS.eval cannot be used.
+    object = JS.global[:Object].call(:call)
+
+    kwargs.each do |(key, value)|
+      object[key] = value
+    end
+
+    object
+  end
 end
 
+content_script site: 'www.example.com' do
+  h1 = document.querySelector("h1")
+  h1.innerText = "Hello unloosen!"
+
+  chrome.runtime.onMessage.addListener do |message|
+    h1 = document.querySelector('h1')
+    text = window.getSelection().toString()
+    h1.innerText = text
+    true
+  end
+end
+
+popup do
+  submit_button = document.querySelector('button#unloosen-button')
+  submit_button.addEventListener "click" do |e|
+    query_object = Utils.build_js_object(active: true, currentWindow: true)
+    chrome.tabs.query(query_object) do |tabs|
+      tab = tabs.at(0)
+
+      chrome.tabs.sendMessage(tab[:id], '')
+    end
+    e.preventDefault
+  end
+end
